b6b981dc5d231692010b73e6591971c328a020d1
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-30 22:57:40 +0100

    markup - single venue page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,14 @@ export default {
         "daze-text": "#141F2A",
         "daze-primary": "#2F4A52",
         "daze-primary-op50": "#909EA2",
+        "daze-primary-op30": "#D6DEDF",
+        "daze-primary-op10": "#CBD0D2",
         "daze-secondary": "#648E94",
         "daze-accent": "#C78D70",
         "daze-accent-op30": "#E5D4CB",
         "daze-white": "#FFFFFF",
-        "daze-gray": "#353535"
+        "daze-gray": "#353535",
+        "daze-red": "#8B0404"
       }
     },
   },
