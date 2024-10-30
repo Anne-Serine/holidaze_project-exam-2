7fbea29ab4db2bddd6716823193cf15a7e28d9bd
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-30 14:43:32 +0100

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ export default {
       container: {
         center: true
       },
+      colors: {
+        "daze-bg": "#F2F2F2",
+        "daze-text": "#141F2A",
+        "daze-primary": "#2F4A52",
+        "daze-primary-op50": "#909EA2",
+        "daze-secondary": "#648E94",
+        "daze-accent": "#C78D70",
+        "daze-accent-op30": "#E5D4CB",
+        "daze-white": "#FFFFFF",
+        "daze-gray": "#353535"
+      }
     },
   },
   plugins: [],
