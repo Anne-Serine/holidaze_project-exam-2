90d4b198841331f0658b7e7c6578993c84796db8
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-31 19:05:53 +0100

    markup - venue management page

--- src/pages/VenueManagement.jsx
+++ src/pages/VenueManagement.jsx
@@ -1,9 +1,22 @@
 function VenueManagement() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="">
+      <hr className="h-10 bg-daze-white" />
+      <h1 className="container-hug text-3xl py-5">New venue</h1>
+      <div className="container-hug grid grid-cols-3">
+        <div className="col-span-2">
+          <form action="">FORM</form>
+        </div>
+        <div className="bg-daze-primary-op10 col-span-1 flex flex-col p-10 text-center justify-center mx-4">
+          <h2 className="pb-5">Lorem ipsum dolor sit amet</h2>
+          <p className="pb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+          <button className="bg-white p-3">CLICK</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VenueManagement;
