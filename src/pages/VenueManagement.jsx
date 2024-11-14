567d1ed8f7cca78586bea4a9da482b12126a093c
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-11-14 13:24:10 +0100

    VenueForm - create component

--- src/pages/VenueManagement.jsx
+++ src/pages/VenueManagement.jsx
@@ -1,22 +1,25 @@
-function VenueManagement() {
+import Button from "../components/common/Buttons";
+import VenueForm from "../components/forms/VenueForm";
+
+function VenueManagementForm() {
   return (
-    <div className="">
+    <div>
       <hr className="h-10 bg-daze-white" />
-      <h1 className="container-hug text-3xl py-5">New venue</h1>
-      <div className="container-hug grid grid-cols-3">
-        <div className="col-span-2">
-          <form action="">FORM</form>
+      <div className="max-w-[50rem] mx-auto px-2 flex flex-wrap gap-5">
+        <div className="flex flex-col flex-1">
+          <h1 className="text-3xl py-5">New venue</h1>
+          <VenueForm />
         </div>
-        <div className="bg-daze-primary-op10 col-span-1 flex flex-col p-10 text-center justify-center mx-4">
+        <div className="bg-daze-gray text-daze-white col-span-1 flex flex-col p-10 text-center justify-center max-w-[18rem]">
           <h2 className="pb-5">Lorem ipsum dolor sit amet</h2>
           <p className="pb-5">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
-          <button className="bg-white p-3">CLICK</button>
+          <Button text="Click" />
         </div>
       </div>
     </div>
   );
 }
 
-export default VenueManagement;
+export default VenueManagementForm;
